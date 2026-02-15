6382fa2e56199efd8e90a0614d381f674132e016
pranavsinhatutu-cell <pranavsinhatutu@gmail.com>
2026-02-15 16:17:58 +0530

    UC3: Add Class

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,19 @@
-public class OOPSBannerApp {
-    public static void main(String[] args) {
-        System.out.println("OOPS");
+class Banner {
+
+    void printBanner() {
+        System.out.println("***********************");
+        System.out.println("      OOPS BANNER      ");
+        System.out.println("***********************");
     }
 }
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        Banner b = new Banner();   // object creation
+        b.printBanner();           // method call
+
+    }
+}
+
