ed305159ed35d95ce319614ec1bae27d08d044e3
pranavsinhatutu-cell <pranavsinhatutu@gmail.com>
2026-02-15 16:39:39 +0530

    UC4: Constructor Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -3,19 +3,16 @@ public class OOPSBannerApp {
     public static void main(String[] args) {
 
         System.out.println("***********************");
-        System.out.println("      OOPS BANNER      ");
+        System.out.println("     OOPS BANNER       ");
+        System.out.println("   Using Constructor   ");
         System.out.println("***********************");
 
     }
-}
-
-public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        Banner b = new Banner();   // object creation
-        b.printBanner();           // method call
+        // object creation
+        OOPSBannerApp obj = new OOPSBannerApp();
 
     }
 }
-
