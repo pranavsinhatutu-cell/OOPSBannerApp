cd231e68ee74b46ff4cd23a83ecabaa34705cb38
pranavsinhatutu-cell <pranavsinhatutu@gmail.com>
2026-02-16 22:56:10 +0530

    save before merging to main

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,26 @@
 public class OOPSBannerApp {
 
+
     public static void main(String[] args) {
 
-        System.out.println("***********************");
-        System.out.println("     OOPS BANNER       ");
-        System.out.println("   Using Constructor   ");
-        System.out.println("***********************");
+        System.out.println("******************************");
+        System.out.println("        OOPS BANNER          ");
+        System.out.println("******************************");
 
+        Banner b = new Banner();
+        b.printBanner();
+    }
+}
+
+class Banner {
+
+    Banner() {
+        System.out.println("Banner Object Created");
     }
 
-    public static void main(String[] args) {
-        System.out.println("OOPS");
+    void printBanner() {
+        System.out.println("******************************");
+        System.out.println("        OOPS BANNER          ");
+        System.out.println("******************************");
     }
 }
